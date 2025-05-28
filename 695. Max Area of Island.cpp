dbbe67b5f4ba2046c6e6f4505ac485b59829ec92
Wayne1895 <waynes1895@gmail.com>
2025-05-28 16:21:41 +0800

    first commit

---
+++ 695. Max Area of Island.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int dfs(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &visited){
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size()) return 0;
+        if (grid[i][j] == 0 || visited[i][j] == 1) return 0;
+        visited[i][j] = 1;
+        int area = 1;
+        
+                
+        area+=dfs(i-1, j, grid, visited);
+        area+=dfs(i+1, j, grid, visited);
+        area+=dfs(i, j-1, grid, visited);
+        area+=dfs(i, j+1, grid, visited);
+            
+    
+        return area;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxarea = 0;
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && visited[i][j]==0){
+                    int area = 0;
+                    maxarea = max(maxarea, dfs(i, j, grid, visited));
+                }
+            }
+        }
+        return maxarea;
+
+        
+    }
+};
